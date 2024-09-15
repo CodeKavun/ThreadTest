03301aa6d090fa98c6f8a4d90de5b7e155458e09
PanDmytryk <dimacapny22@gmail.com>
2024-09-15 20:10:19 +0200

    Add project files.

---
+++ ThreadTest/Bank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    internal class Bank
+    {
+        public int Money { get; set; }
+        public string Name { get; set; }
+        public int Percent { get; set; }
+
+        private readonly Thread thread;
+
+        public Bank(int money, string name, int percent)
+        {
+            ThreadStart threadStart = new ThreadStart(WriteIntoTextFile);
+            thread = new Thread(threadStart);
+
+            Money = money;
+            Name = name;
+            Percent = percent;
+        }
+
+        public void SetInfo(int money, string name, int percent)
+        {
+            Money = money;
+            Name = name;
+            Percent = percent;
+
+            thread.Start();
+        }
+
+        private void WriteIntoTextFile()
+        {
+            using (StreamWriter writer = new StreamWriter("bank.txt", false, Encoding.UTF8))
+            {
+                string data = $"Money = {Money}\nName = {Name}\nPercent = {Percent}";
+                writer.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ ThreadTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            ParameterizedThreadStart threadCallback = new ParameterizedThreadStart(IterateThrouhElements);
+            Thread collectionThread = new Thread(threadCallback);
+
+            collectionThread.Start(new int[] { 3, 12, 56, 222, 333, 666, 777, 12321 });
+
+            Console.ReadKey();
+
+            // 2
+            Bank bank = new Bank(1500, "DIMON", 70);
+            bank.SetInfo(1300, "DIMON", 70);
+        }
+
+        static void IterateThrouhElements(object collection)
+        {
+            int[] objects = (int[])collection;
+            foreach (object obj in objects) Console.WriteLine(obj.ToString());
+        }
+    }
+}
